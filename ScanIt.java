49555e1f34953120e3e17ac8590bfe3cb04c948f
Rahul Mitra <rmitra@trincoll.edu>
2019-09-11 21:40:01 -0400

    Added comments

--- ScanIt.java
+++ ScanIt.java
@@ -1,8 +1,12 @@
+/**
+ * Rahul Mitra
+ * CPSC316, Assignment 1 
+ * A Simple Scanner
+ */
 import java.io.*;
-import java.util.*;
 public class ScanIt {
     public static void main(String[] args)throws Exception {
-        if (args.length == 0){
+        if (args.length == 0){//if no file is passed on the command line
             System.out.println("No command line arguments were passed.");
             System.exit(0);
         }
@@ -39,16 +43,16 @@ public class ScanIt {
                             num += line.charAt(i);
                             i++;
                         }
-                        if ( num.indexOf('.') == - 1){//there is no decimal 
+                        if ( num.indexOf('.') == - 1){//there is no decimal so it is a integer
                             System.out.println(k+": "+"integer"+" '"+ num +"' "+"found");
                             System.out.println();
                         }
-                        else{//there is a decimal
+                        else{//there is a decimal so it is a float
                             System.out.println(k+": "+"float"+" '"+ num +"' "+"found");
                             System.out.println();
                         }
                     }
-                    else if (isOperator(line.charAt(i))){
+                    else if (isOperator(line.charAt(i))){//handles operators
                         System.out.println(k+": "+"operator"+" '"+line.charAt(i)+"' "+"found");
                         System.out.println();
                         i++;
@@ -57,11 +61,11 @@ public class ScanIt {
                         i = i + 1;
                         String newstring = "";
                         while(line.charAt(i) != '"'){
-                            if ( line.charAt(i) == '\\' && line.charAt(i + 1) == '\\'){
+                            if ( line.charAt(i) == '\\' && line.charAt(i + 1) == '\\'){//if it's an escape character for '\'
                                 newstring += '\\';
                                 i = i + 2;
                             }
-                            else if ( line.charAt(i) == '\\' && line.charAt(i + 1) == '"'){
+                            else if ( line.charAt(i) == '\\' && line.charAt(i + 1) == '"'){//if it's an escape character for '"'
                                 i = i + 1;
                             }
                             newstring += line.charAt(i);
@@ -71,20 +75,25 @@ public class ScanIt {
                         System.out.println();
                         i = i + 1;
                     }
-                    else{
-                        if ( line.charAt(i) != ' '){
+                    else{//handles unknown characters 
+                        if ( line.charAt(i) != ' '){//if it's not a blank space then it's an unknown character
                             System.out.println(k+": "+"unknown character"+" 0x"+Integer.toHexString((int)line.charAt(i)));
                             System.out.println();
                         }
                         i++;
                     }
                 }
-                k++;
+                k++;//increment line number
                 line = br.readLine();
             }
             br.close();
-        }
-    }
+        }//main
+    }//class
+    /**
+     * This function checks if a particular character is an operator
+     * @param ch character to test
+     * @return true if character is an operator or false otherwise
+     */
     public static boolean isOperator(char ch){
         if(ch == '&' || ch == '=' || ch == '!' || ch == ':' || ch == ',' || ch == '.' || ch == '>' || ch == '<' || ch == '[' || ch == ']' || ch == '(' || ch == ')' || ch == '+' || ch == '-' || ch == '/' || ch == '*' || ch == ';') {
             return true;
--- prog.x
+++ prog.x
@@ -1,7 +1,5 @@
-"\H"
-i = i + 10@
-print("Hello, World."); 
+i = i + 1@
+print("Hello, World.");
 print("John said, \"Hello.\"");
 print("My file is located in C:\\Scanner\\Project");
 x = 0.25 * .1..23
-
